f59d25ccc8165aa6ef063b04c3822b718855bdc1
Barré <barre.alexandre44@outlook.fr>
2019-11-19 17:38:04 +0100

    quest 05 finis

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends AbstractController
      */
     public function index() :Response
     {
-        return $this->render('home.html.twig', [
+        return $this->render('wild/home.html.twig', [
             'message' => 'Bienvenue'
         ]);
     }
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,15 +7,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() :Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries'
+        return $this->render('index.html.twig', [
+            'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[0-9a-z-]+"},
+     * methods={"GET"}, defaults={"slug": "Aucune série sélectionnée, veuillez choisir une série"}, name="show")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug) :Response
+    {
+        try
+        {
+            return $this->render('wild/show.html.twig', [
+                'slug' => str_replace('-', ' ', ucwords($slug, '-')),
+            ]);
+        }
+        catch (\Exception $error){
+            $error->getMessage("erreur 404");
+        }
     }
 }
