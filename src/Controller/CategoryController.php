9f71b680eb4da35eb957f30ca871e56175532ea2
Barré <barre.alexandre44@outlook.fr>
2019-12-12 16:16:43 +0100

    quest13 finish

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
-use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
